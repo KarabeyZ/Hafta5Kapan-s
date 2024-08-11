9b2da58145ac256de941dfa3e8ac16e168111106
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-11 17:59:22 +0300

    Add project files.

---
+++ Hafta5Kapanıs/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta5Kapanıs
+{
+    public class Car
+    {
+        private int _serialNo { get; set; }
+        private string _brand { get; set; }
+        private int _model {  get; set; }
+        private string _color { get; set; }
+        private int _door { get; set; }
+        DateTime _date { get; set; }
+        public Car(int serialNo, string brand, int model, string color, int door,DateTime date)
+        {
+            _serialNo = serialNo;
+            _brand = brand;
+            _model = model;
+            _color = color;
+            _door = door;
+            _date = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"Marka: {_brand}, Model: {_model}, Seri No: {_serialNo}, Renk: {_color}, Kapı Sayısı: {_door}, Üretim Tarihi: {DateTime.Now}";
+        }
+    }
+    
+}
---
+++ Hafta5Kapanıs/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using Hafta5Kapanıs;
+List<Car> carList = new List<Car>();
+while (true)
+{
+    
+        Console.WriteLine("Araba üretmek istiyor musunuz?(e/h)");
+        string userAnswer = Console.ReadLine().Trim().ToLower();
+        if (userAnswer == "e")
+        {
+            Console.WriteLine("Araba markası girin: ");
+            string carBrand = Console.ReadLine();
+            Console.WriteLine("Araba modeli girin: ");
+            int carModel = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Araba seri no girin: ");
+            int carSerial = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Araba rengi girin: ");
+            string carColor = Console.ReadLine();
+            int carDoor = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Kapı sayısını girin 2 veya 4 olmalıdır :");
+                    carDoor = Convert.ToInt32(Console.ReadLine());
+                    if (carDoor == 2 || carDoor == 4)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kapı sayısı sadece 2 veya 4 olabilir.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Kapı sayısı sayısal bir değer olmalıdır.");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hatali giris yaptiniz.");
+                }
+            }
+            Car car = new Car(carSerial, carBrand, carModel, carColor, carDoor, DateTime.Now);
+            carList.Add(car);
+        }
+        else if (userAnswer == "h")
+        {
+            
+                foreach (var cars in carList)
+                {
+                    Console.WriteLine(cars.ToString());
+                }
+
+                break;
+            
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz cevap, lütfen 'e' ya da 'h' olarak cevaplayın ");
+        }
+}
+
+
